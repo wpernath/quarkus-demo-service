1702864774a55d9577d560b1b437d11ca494195f
Wanja Pernath <wpernath@redhat.com>
2023-07-10 17:18:53 +0200

    Adding quarkus-3.2 files

--- src/main/java/org/wanja/demo/Person.java
+++ src/main/java/org/wanja/demo/Person.java
@@ -1,19 +1,12 @@
 package org.wanja.demo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
 
 @Entity
 public class Person extends PanacheEntity {
-
-    @Column(name = "first_name")
     public String firstName;
-
-    @Column(name = "last_name")
     public String lastName;
     public String salutation;
-
     
 }
--- src/main/java/org/wanja/demo/PersonResource.java
+++ src/main/java/org/wanja/demo/PersonResource.java
@@ -2,56 +2,81 @@ package org.wanja.demo;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
 
 @Path("/persons")
-
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Person API",
+        version= "0.11",
+        contact = @Contact(
+            name = "Person API Support",
+            url = "https://github.com/wpernath/quarkus-demo-service",
+            email="wanja@example.org"
+        ),
+        license = @License(
+            name = "Apache Software License 2.0",
+            url  = "https://www.apache.org/licenses/LICENSE-2.0.html"
+        )
+    ))
 public class PersonResource {
     
-    @GET
-    @Path("{id}")
-    @Produces("text/plain")
-    public String personName(Long id) {
-        Person p = Person.findById(id);
-        return p.salutation + " " + p.firstName + " " + p.lastName;
-    }
-
-
+    @Operation(
+        summary = "Getting all persons, unsorted",
+        description = "Method will return an unsorted set of persons currently in the database"
+    )
     @GET
     public List<Person> allPersons() {
         return Person.listAll();
     }
 
+    @Operation(
+        summary = "Creating a new person",
+        description = "Method to create a new person in the internal database"
+    )
     @POST
     @Transactional
-    public Person createPerson(Person p) {
+    public void create(Person p) {
+        p.id = null;
+        p.persist();
+    }
+
+    @Operation(
+        summary = "Updating an existing person",
+        description = "Method to update an existing person in the internal database"
+    )
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Person update(Long id, Person per) {
+        Person p = Person.findById(id);
+        if( per.firstName != null ) p.firstName = per.firstName;
+        if( per.lastName != null )  p.lastName  = per.lastName;
+        if( per.salutation != null )p.salutation= per.salutation;
         p.persist();
         return p;
     }
 
-    @PUT
+    @Operation(
+        summary = "Deleting a person",
+        description = "Method to delete a person from the internal database"
+    )
+    @DELETE
     @Transactional
     @Path("{id}")
-    public Person updatePerson(Long id, Person p) {
-        Person person = Person.findById(id);
-        if( p.firstName != null ) {
-            person.firstName = p.firstName;
-        }
-
-        if (p.lastName != null) {
-            person.lastName = p.lastName;
-        }
-        
-        if( p.salutation != null ) {
-            person.salutation = p.salutation;
-        }
-
-        person.persist();
-        return person;
+    public void delete(Long id) {
+        Person p = Person.findById(id);
+        if( p != null ) p.delete();
     }
 }
