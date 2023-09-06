b917d0a7d4f399b3d28bc5c39c0aed349973266c
Wanja Pernath <wpernath@redhat.com>
2023-09-06 13:30:27 +0200

    removing oidc connect

--- src/main/java/org/wanja/demo/ReportResource.java
+++ src/main/java/org/wanja/demo/ReportResource.java
@@ -4,7 +4,6 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import io.quarkus.security.Authenticated;
 import io.smallrye.common.annotation.Blocking;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -13,7 +12,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/reports")
-@Authenticated
+
 public class ReportResource {
     
 
